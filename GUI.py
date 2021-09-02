9b72eb8d9036da6c11882df323ae048f092fb481
Tyrnan Batch <51964350+TyrnanBatch@users.noreply.github.com>
2021-09-02 16:06:54 +0100

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,52 +1,50 @@
-import tkinter as tk
-import time
+def master_password_entry():
+    import tkinter as tk
+    import time
 
-root = tk.Tk()
-root.configure(bg = 'grey')
-root.title('Password Manager')
+    from masterpassword import master_password_func
 
-from masterpassword import master_password_func
+    root = tk.Tk()
+    root.configure(bg = 'grey')
+    root.title('Password Manager')
 
-show_text = '*'
-submit_button_colour = 'black'
+    def master_password_read():
+        result = master_password_input.get()
+        if master_password_func(result):
+            root.destroy()
+            return True
+        else:
+            print('INCORRECT')
 
-def master_password_read():
-    result = master_password_input.get()
-    if master_password_func(result):
-        root.destroy()
-    else:
-        submit_button_colour = 'red'
-        time.sleep(0.3)
-        submit_button_colour = 'black'
 
-def show_button():
-    global show_text
-    if show_text == '*':
-        show_text = ''
-    elif show_text ==  '':
-        show_text = '*'
-    print(master_password_input)
-    root.update_idletasks()
-    root.update()
+    try:
+        file = open("storage/masterpassword.txt","r")
+        master_password = file.read()
+        file.close()
+        request_text = 'Enter new Master Password: '
+    except:
+        request_text = 'Enter Master Password: '
 
-try:
-    file = open("storage/masterpassword.txt","r")
-    master_password = file.read()
-    file.close()
-    request_text = 'Enter new Master Password: '
-except:
-    request_text = 'Enter Master Password: '
+    request = tk.Label(root, font = 'Times 20 bold italic', width = 21, height = 2, text = request_text)
+    request.grid(row = 1, column = 1,)
 
-request = tk.Label(root, font = 'Times 20 bold italic', width = 62, text = request_text)
-request.grid(row = 1, column = 0, columnspan = 2)
+    master_password_input = tk.Entry(root, justify = 'center', width = 25, font = 'Times 40 bold italic', bd = 5, show = '*')
+    master_password_input.grid(row = 1, column = 2)
 
-master_password_input = tk.Entry(root, justify = 'center', width = 30, font = 'Times 40 bold italic', bd = 5, show = show_text)
-master_password_input.grid(row = 2, column = 0)
+    master_password_submit = tk.Button(root, height = 1, width = 44, bd = 10, text = 'SUBMIT', font = 'Times 30 bold italic', command = master_password_read)
+    master_password_submit.grid(row = 2, column = 1, columnspan = 2)
 
-show_button = tk.Button(root, fg = submit_button_colour, height = 2, width = 12, bd = 10, text = 'show', font = 'Times 16 bold italic', command = show_button)
-show_button.grid(row = 2, column = 1)
+    master_password_reset = tk.Button(root, height = 6, text = 'Reset Master Password', font = 'Times 15 bold italic', bd = 10)
+    master_password_reset.grid(row = 1, column = 3, rowspan = 2)
 
-master_password_submit = tk.Button(root, height = 2, width = 42, bd = 10, text = 'Submit', font = 'Times 30 bold italic', command = master_password_read)
-master_password_submit.grid(row = 3, column = 0, columnspan = 2)
+    root.mainloop()
 
-root.mainloop()
+def password_manager_gui():
+
+    import tkinter as tk
+
+    from functions import quit_func
+
+    root = tk.Tk()
+
+    quit_button = tk.Button(root, command = quit_func).grid(row = 1, column = 1)
